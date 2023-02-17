fb73ae42f0f5566d81ce0d77ea006446d45ab5f3
sam1rC <samir.colpas@hotmail.com>
2023-02-17 16:20:15 -0500

    add dynamic size

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
 const gridSizeText = document.getElementById('gridSizeText');
 const gridSize = document.getElementById('gridSize');
-gridSize.onmousemove = (e)=> {updateGridSizeText(e.target.value)};
+const grid = document.querySelector('.container');
+
+
+gridSize.addEventListener('mousemove',(e)=>{updateGridSizeText(e.target.value)});
+gridSize.addEventListener('mouseup',(e)=>{upgradeGrid(e.target.value)});
+
 
 function updateGridSizeText(value)
 {
-    gridSizeValueText.textContent = `${value} x ${value}`;
+    gridSizeText.textContent = `${value} x ${value}`;
+}
+
+function upgradeGrid(value)
+{   
+    removeAllChilds();
+    for(let i=0;i<value*value;i++)
+    {
+        const gridChild = document.createElement('div');
+        gridChild.setAttribute('style',`width: calc(100%/${value});height: calc(100%/${value}); border: 1px solid red;`);
+        grid.appendChild(gridChild);
+    }
+}
+
+
+function removeAllChilds()
+{
+    while(grid.firstChild)
+    {
+        grid.removeChild(grid.firstChild);
+    }
 }
